5e866b4da9f3ebaa28df5023c227ef4f1d9f1586
22PROGB07 <22PROGB07@TS-LABS-5.labs.cbtis94.local>
2023-11-27 09:33:06 -0600

    VENTANA7

---
+++ src/ventanas/NoPersonas.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ventanas;
+
+/**
+ *
+ * @author 22PROGB07
+ */
+public class NoPersonas {
+    private String DolorDeCabeza;
+    private String GolpesOTorceduras;
+    private String InfeccionOjos;
+    private String Influenza;
+    private String DoloresMenstruales;
+    private String DoloresEstomacales;
+    private String Gripe;
+    private String Embarazo;
+    private String Total;
+
+    public NoPersonas(String DolorDeCabeza, String GolpesOTorceduras, String InfeccionOjos, String Influenza, String DoloresMenstruales, String DoloresEstomacales, String Gripe, String Embarazo, String Total) {
+        this.DolorDeCabeza = DolorDeCabeza;
+        this.GolpesOTorceduras = GolpesOTorceduras;
+        this.InfeccionOjos = InfeccionOjos;
+        this.Influenza = Influenza;
+        this.DoloresMenstruales = DoloresMenstruales;
+        this.DoloresEstomacales = DoloresEstomacales;
+        this.Gripe = Gripe;
+        this.Embarazo = Embarazo;
+        this.Total = Total;
+    }
+
+    public String getDolorDeCabeza() {
+        return DolorDeCabeza;
+    }
+
+    public void setDolorDeCabeza(String DolorDeCabeza) {
+        this.DolorDeCabeza = DolorDeCabeza;
+    }
+
+    public String getGolpesOTorceduras() {
+        return GolpesOTorceduras;
+    }
+
+    public void setGolpesOTorceduras(String GolpesOTorceduras) {
+        this.GolpesOTorceduras = GolpesOTorceduras;
+    }
+
+    public String getInfeccionOjos() {
+        return InfeccionOjos;
+    }
+
+    public void setInfeccionOjos(String InfeccionOjos) {
+        this.InfeccionOjos = InfeccionOjos;
+    }
+
+    public String getInfluenza() {
+        return Influenza;
+    }
+
+    public void setInfluenza(String Influenza) {
+        this.Influenza = Influenza;
+    }
+
+    public String getDoloresMenstruales() {
+        return DoloresMenstruales;
+    }
+
+    public void setDoloresMenstruales(String DoloresMenstruales) {
+        this.DoloresMenstruales = DoloresMenstruales;
+    }
+
+    public String getDoloresEstomacales() {
+        return DoloresEstomacales;
+    }
+
+    public void setDoloresEstomacales(String DoloresEstomacales) {
+        this.DoloresEstomacales = DoloresEstomacales;
+    }
+
+    public String getGripe() {
+        return Gripe;
+    }
+
+    public void setGripe(String Gripe) {
+        this.Gripe = Gripe;
+    }
+
+    public String getEmbarazo() {
+        return Embarazo;
+    }
+
+    public void setEmbarazo(String Embarazo) {
+        this.Embarazo = Embarazo;
+    }
+
+    public String getTotal() {
+        return Total;
+    }
+
+    public void setTotal(String Total) {
+        this.Total = Total;
+    }
+    
+}
